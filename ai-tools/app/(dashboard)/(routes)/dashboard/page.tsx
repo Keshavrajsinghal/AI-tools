3e092530f922b85461678e9f66c982d65344494c
Keshav <keshav17@berkeley.edu>
2023-08-09 00:36:33 -0700

    Created sidebar

--- ai-tools/app/(dashboard)/(routes)/dashboard/page.tsx
+++ ai-tools/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function DashboardPage() {
     return (
         <div>
             <p>Dashboard Page (Authorized)</p>
-            <UserButton afterSignOutUrl="/" />
+
         </div>
     );
 }
